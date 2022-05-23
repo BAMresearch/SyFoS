3bb2f8effced894cdfdd6a1a9d643afbe686c24e
Silbernagl <dorothee.silbernagl@bam.de>
2022-05-23 14:56:52 +0200

    changes for sphynx

--- docs/make.bat
+++ docs/make.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD="C:\Users\dsilbern\AppData\Roaming\Python\Python38\Scripts\sphinx-build.exe"
 )
 set SOURCEDIR=source
 set BUILDDIR=build
